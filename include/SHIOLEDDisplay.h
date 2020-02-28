65c52aa19d768d6efe33e35329cb517b389375fc
Karsten Becker <kb@karsten-becker.de>
2020-02-28 19:43:16 +0100

    First version of standalone SHI OLED

---
+++ include/SHIOLEDDisplay.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2020 Karsten Becker All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+#pragma once
+#include <Arduino.h>
+
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "SHICommunicator.h"
+
+// namespace std {
+// template <>
+// struct hash<String> {
+//   std::size_t operator()(String const &s) const noexcept {
+//     return std::hash<std::string>{}(s.c_str());
+//   }
+// };
+// }  // namespace std
+
+namespace SHI {
+
+class OLEDDisplay : public Communicator {
+ public:
+  OLEDDisplay(std::pair<std::string, String> firstRow = {"", ""},
+              std::pair<std::string, String> secondRow = {"", ""},
+              std::pair<std::string, String> thirdRow = {"", ""})
+      : Communicator("OLEDDisplay") {
+    displayItems.insert({firstRow.first, 0});
+    displayItems.insert({secondRow.first, 1});
+    displayItems.insert({thirdRow.first, 2});
+    displayLineBuf[0] = firstRow.second;
+    displayLineBuf[2] = secondRow.second;
+    displayLineBuf[4] = thirdRow.second;
+  }
+  void setupCommunication() override;
+  void loopCommunication() override;
+  void newReading(const MeasurementBundle &reading) override;
+  void newStatus(const Measurement &status, SHIObject *src) override;
+  void setBrightness(uint8_t level);
+
+ private:
+  std::unordered_map<std::string, int> displayItems;
+  String displayLineBuf[7] = {"", "", "", "", "", "", ""};
+};
+
+}  // namespace SHI
---
+++ src/SHIOLEDDisplay.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2020 Karsten Becker All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+#include "SHIOLEDDisplay.h"
+
+#include <Arduino.h>
+#include <oled/SSD1306Wire.h>
+
+#include "SHIHardware.h"
+
+namespace {
+SSD1306Wire display =
+    SSD1306Wire(0x3c, SDA_OLED, SCL_OLED, RST_OLED, GEOMETRY_128_64);
+bool displayUpdated = false;
+}  // namespace
+
+void SHI::OLEDDisplay::setupCommunication() {
+  display.init();
+  display.flipScreenVertically();
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 0, "OLED initial done!");
+  display.setBrightness(5);
+  display.display();
+  SHI::hw->feedWatchdog();
+}
+
+void SHI::OLEDDisplay::loopCommunication() {
+  if (displayUpdated) {
+    displayUpdated = false;
+    display.clear();
+    for (int i = 0; i < 6; i += 2)
+      display.drawString(0, (i / 2) * 13, displayLineBuf[i]);
+    for (int i = 1; i < 6; i += 2)
+      display.drawString(90, (i / 2) * 13, displayLineBuf[i]);
+    display.drawStringMaxWidth(0, 3 * 13, 128, displayLineBuf[6]);
+    display.display();
+  }
+}
+
+void SHI::OLEDDisplay::newReading(const SHI::MeasurementBundle &reading) {
+  const std::string baseName = reading.src->getName();
+  for (auto &&data : reading.data) {
+    auto sensorName = baseName + data.getMetaData()->getName();
+    auto value = displayItems.find(sensorName);
+    if (value != displayItems.end()) {
+      displayLineBuf[(value->second * 2) + 1] =
+          String(data.toTransmitString().c_str());
+      displayUpdated = true;
+    }
+  }
+}
+
+void SHI::OLEDDisplay::newStatus(const Measurement &status, SHIObject *src) {
+  if (status.stringRepresentation != STATUS_OK) {
+    displayLineBuf[6] = String(status.stringRepresentation.c_str());
+    displayUpdated = true;
+  }
+}
+
+void SHI::OLEDDisplay::setBrightness(uint8_t level) {
+  display.setBrightness(level);
+}
